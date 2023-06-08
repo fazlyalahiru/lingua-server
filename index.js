ab46318b7b03797b6060459a74edc28f7d16febc
fazlyalahiru <fazlyalahi.ru@gmail.com>
2023-06-08 22:06:05 +0600

    mongodb setup done

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
 const express = require('express');
 const app = express()
 const cors = require('cors');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // Middleware 
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req, res)=>{
+//MongoDB starts here
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ywgzzs0.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        const usersCollenction = client.db('linguaDb').collection('users')
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Amazing! you just hit the MongoDB");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+// MongoDB ends here
+
+app.get('/', (req, res) => {
     res.send('Langua server is running')
 })
 
-app.listen(port, (req, res)=>{
+app.listen(port, (req, res) => {
     console.log(`the server is running on ${port}`);
 })
