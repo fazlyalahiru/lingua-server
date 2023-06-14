888415b3aebf82e8e58bd826676a8e1f14a1d126
fazlyalahiru <fazlyalahi.ru@gmail.com>
2023-06-14 23:31:01 +0600

    some api modified

--- index.js
+++ index.js
@@ -16,10 +16,8 @@ app.use(morgan('tiny'))
 
 //MongoDB starts here
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ywgzzs0.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -188,8 +186,7 @@ async function run() {
         // find deniyed classes
         app.post('/classes/deny/:id', async (req, res) => {
             const classId = req.params.id;
-            // const statusMessage = req.body.textBoxValue;
-            // console.log(statusMessage);
+           
             try {
                 const result = await classCollection.updateOne(
                     { _id: new ObjectId(classId) },
@@ -346,10 +343,6 @@ async function run() {
 
         })
 
-
-        // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect();
-        // Send a ping to confirm a successful connection
         client.db("admin").command({ ping: 1 });
         console.log("Amazing! you just hit the MongoDB");
     } finally {
